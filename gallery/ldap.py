1645186901801fbc8b98254a921877b83092249a
Liam Middlebrook <liammiddlebrook@gmail.com>
2017-03-15 01:03:16 -0400

    root passthrough

--- gallery/ldap.py
+++ gallery/ldap.py
@@ -4,4 +4,6 @@ from gallery import ldap
 
 @lru_cache(maxsize=1024)
 def ldap_convert_uuid_to_displayname(uuid):
+    if uuid == "root":
+        return uuid
     return ldap.get_member(uuid).displayName
