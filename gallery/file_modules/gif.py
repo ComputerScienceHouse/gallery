647b937157f497c1294056df1c1d51e19cab4314
Devin Matte <devinmatte@gmail.com>
2017-09-17 18:38:14 -0400

    Properly making gif Thumbnail

--- gallery/file_modules/gif.py
+++ gallery/file_modules/gif.py
@@ -1,15 +1,24 @@
 import os
-import piexif
+
 from wand.image import Image
-from wand.color import Color
 
 from gallery.file_modules import FileModule
 from gallery.util import hash_file
 
-class GIFFile(FileModule):
 
+class GIFFile(FileModule):
     def __init__(self, file_path):
         FileModule.__init__(self, file_path)
         self.mime_type = "image/gif"
 
         self.generate_thumbnail()
+
+    def generate_thumbnail(self):
+        self.thumbnail_uuid = hash_file(self.file_path) + ".gif"
+
+        with Image(filename=self.file_path) as img:
+            size = img.width if img.width < img.height else img.height
+            img.crop(width=size, height=size, gravity='center')
+            img.resize(256, 256)
+            img.format = 'gif'
+            img.save(filename=os.path.join("/gallery-data/thumbnails", self.thumbnail_uuid))
