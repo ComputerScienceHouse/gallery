ComputerScienceHouse/gallery

commit c37ad204f98611af161940f0c1e5e9827e65426b

Implement move functionality for dirs and files

gallery/static/js/gallery.js
@@ -233,6 +233,40 @@ function deleteFile() {
     });
 }
 
+function moveFile() {
+    $('#move').modal('show');
+    $('#move button[id^="move"]').click(function(e) {
+        e.preventDefault();
+        var this_id = $('#move button[id^="move"]').attr('id').substr($('#move button[id^="move"]').attr('id').indexOf("-") + 1);
+        if ($('#move input[name="gallery_dir_id"]').length) {
+            $.ajax({
+                type: "POST",
+                url: "/api/file/move/" + this_id,
+                data: {
+                    parent: $('#move input[name="gallery_dir_id"]').val()
+                }
+            });
+        }
+    });
+}
+
+function moveDir() {
+    $('#move').modal('show');
+    $('#move button[id^="move"]').click(function(e) {
+        e.preventDefault();
+        var this_id = $('#move button[id^="move"]').attr('id').substr($('#move button[id^="move"]').attr('id').indexOf("-") + 1);
+        if ($('#move input[name="gallery_dir_id"]').length) {
+            $.ajax({
+                type: "POST",
+                url: "/api/dir/move/" + this_id,
+                data: {
+                    parent: $('#move input[name="gallery_dir_id"]').val()
+                }
+            });
+        }
+    });
+}
+
 function kbGalleryVideoSelect(e) {
     var video = $('video');
     if (video.length > 0) {

gallery/templates/view_dir.html
@@ -5,6 +5,7 @@ View Directory
 
 {% block extraHead %}
 <link rel="stylesheet" href="https://rawgit.com/enyo/dropzone/master/dist/dropzone.css">
+<link href="https://mbraak.github.io/jqTree/jqtree.css" rel="stylesheet">
 {% endblock %}
 {% block body %}
 
@@ -73,6 +74,7 @@ View Directory
     <div class="col-xs-12 text-center">
         <a href="#!" class="btn btn-primary" onclick="editDirDescription()">Edit{{ '' if auth_dict['can_edit'] else ' Description' }}</a>
         {% if auth_dict['can_edit'] %}
+        <a href="#!" class="btn btn-warning" onclick="moveDir()">Move</a>
         <a href="#!" class="btn btn-danger" onclick="deleteDir()">Delete</a>
         {% endif %}
     </div>
@@ -103,6 +105,25 @@ View Directory
     </div>
 
     {% if auth_dict['can_edit'] %}
+    <div class="modal fade" id="move" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    <h4 class="modal-title">Select a new parent folder:</h4>
+                    <div id="fileList"></div>
+                    <div class="form-group">
+                        <input type="number" name="gallery_dir_id" readonly hidden/>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" id="move-{{ directory.id }}" class="btn btn-primary" data-dismiss="modal">Move</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <div class="modal fade" id="delete" role="dialog">
         <div class="modal-dialog modal-sm">
             <div class="modal-content">
@@ -177,8 +198,12 @@ View Directory
 {% endblock %}
 
 {% block extraFooter %}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqtree/1.3.5/tree.jquery.min.js"></script>
 <script src="https://rawgit.com/enyo/dropzone/master/dist/dropzone.js"></script>
 <script>
+    $(function() {
+        populateDirTree();
+    });
     var mode = "VIEW_DIR";
     var parent = {{ parent }};
 </script>

gallery/__init__.py
@@ -401,6 +401,56 @@ def delete_dir(dir_id, auth_dict=None):
 
     return "ok", 200
 
+@app.route("/api/file/move/<int:file_id>", methods=['POST'])
+@auth.oidc_auth
+@gallery_auth
+def move_file(file_id, auth_dict=None):
+    file_id = int(file_id)
+    file_model = File.query.filter(File.id == file_id).first()
+
+    if file_model is None:
+        return "file not found", 404
+
+    parent = request.form.get('parent')
+
+    if not (auth_dict['is_eboard']
+            or auth_dict['is_rtp']
+            or auth_dict['uuid'] == file_model.author):
+        return "Permission denied", 403
+
+    File.query.filter(File.id == file_id).update({
+        'parent': parent
+    })
+    db.session.flush()
+    db.session.commit()
+
+    return "ok", 200
+
+@app.route("/api/dir/move/<int:dir_id>", methods=['POST'])
+@auth.oidc_auth
+@gallery_auth
+def move_dir(dir_id, auth_dict=None):
+    dir_id = int(dir_id)
+    dir_model = Directory.query.filter(Directory.id == dir_id).first()
+
+    if dir_model is None:
+        return "dir not found", 404
+
+    parent = request.form.get('parent')
+
+    if not (auth_dict['is_eboard']
+            or auth_dict['is_rtp']
+            or auth_dict['uuid'] == dir_model.author):
+        return "Permission denied", 403
+
+    Directory.query.filter(Directory.id == dir_id).update({
+        'parent': parent
+    })
+    db.session.flush()
+    db.session.commit()
+
+    return "ok", 200
+
 @app.route("/api/file/rename/<int:file_id>", methods=['POST'])
 @auth.oidc_auth
 @gallery_auth

gallery/static/css/styles.css
@@ -176,8 +176,8 @@ input#upload {
     margin-top: 20px;
 }
 
-.fileList {
-    margin-bottom: 20px;
+#fileList {
+    text-align: left;
 }
 
 #upload-alert,

gallery/templates/view_file.html
@@ -3,6 +3,10 @@
 View File
 {% endblock %}
 
+{% block extraHead %}
+<link href="https://mbraak.github.io/jqTree/jqtree.css" rel="stylesheet">
+{% endblock %}
+
 {% block body %}
 
 <div class="container">
@@ -105,6 +109,27 @@ View File
                 </div>
             {% endif %}
             {% if auth_dict['can_edit'] %}
+                <a href="#!" class="btn btn-warning" onclick="moveFile()">Move</a>
+
+                <div class="modal fade" id="move" role="dialog">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                                <h4 class="modal-title">Select a new parent folder:</h4>
+                                <div id="fileList"></div>
+                                <div class="form-group">
+                                    <input type="number" name="gallery_dir_id" readonly hidden/>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" id="move-{{ file.id }}" class="btn btn-primary" data-dismiss="modal">Move</button>
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <a href="#!" class="btn btn-danger" onclick="deleteFile()">Delete</a>
 
                 <div class="modal fade" id="delete" role="dialog">
@@ -130,7 +155,11 @@ View File
 {% endblock %}
 
 {% block extraFooter %}
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jqtree/1.3.5/tree.jquery.min.js"></script>
 <script>
+    $(function() {
+        populateDirTree();
+    });
     var mode = "VIEW_FILE";
     var parent = {{ parent }};
     var tags = [];

commit 2df5665940c244e33a576ae53ab57514e0dd461b

Fix directory creation JS/HTML
Commit c37ad204 broke the directory creation JS logic because
populateDirTree clobbers over the value of gallery_dir_id. As a suitable
WAR I just changed the referenced name for directory creation to
gallery_mkdir_id.

gallery/static/js/gallery.js
@@ -55,13 +55,13 @@ function afterMkdir(data) {
 
 // Create a new directory
 function createDirectory() {
-    if ($('input[name="gallery_dir_id"]').val().length > 0) {
+    if ($('input[name="gallery_mkdir_id"]').val().length > 0) {
         $.ajax({
             type: "POST",
             url: "/api/mkdir",
             data: {
                 dir_name: $('input[name="gallery_dir_name"]').val(),
-                parent_id: $('input[name="gallery_dir_id"]').val()
+                parent_id: $('input[name="gallery_mkdir_id"]').val()
             },
             success: afterMkdir
         });

gallery/templates/view_dir.html
@@ -147,7 +147,7 @@ View Directory
             <div class="modal-body text-left">
                 <div class="form-group">
                     <div class="input-group">
-                        <input type="number" name="gallery_dir_id" value="{{directory.id}}" readonly hidden/>
+                        <input type="number" name="gallery_mkdir_id" value="{{directory.id}}" readonly hidden/>
                         <label class="control-label" for="gallery_dir_name">Folder name:</label>
                         <input class="form-control" type="text" id="gallery_dir_name" name="gallery_dir_name" placeholder="Welcome Back, 2017-2018/Fall Camping, etc.">
                         <span class="input-group-btn">