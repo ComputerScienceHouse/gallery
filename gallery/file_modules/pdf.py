103829abb8d6d564a10ff8f09834d6e84025a9a4
Ram Zallan <ramzallan@gmail.com>
2020-06-29 13:59:18 -0400

    Generate thumbnails for PDFs from first page

--- gallery/file_modules/pdf.py
+++ gallery/file_modules/pdf.py
@@ -1,4 +1,9 @@
+import os
+from wand.image import Image
+from wand.color import Color
+
 from gallery.file_modules import FileModule
+from gallery.util import hash_file
 
 
 class PDFFile(FileModule):
@@ -8,3 +13,17 @@ class PDFFile(FileModule):
         self.mime_type = "application/pdf"
 
         self.generate_thumbnail()
+
+    def generate_thumbnail(self):
+        self.thumbnail_uuid = hash_file(self.file_path) + ".jpg"
+
+        with Image(filename=self.file_path, resolution=300) as all_pages:
+            with Image(all_pages.sequence[0]) as first_page:
+                with Image(width=first_page.width, height=first_page.height,
+                        background=Color("#EEEEEE")) as bg:
+                    bg.composite(first_page, 0, 0)
+                    size = first_page.width if first_page.width < first_page.height else first_page.height
+                    bg.crop(width=size, height=size, gravity='north') # top of first page
+                    bg.resize(256, 256)
+                    bg.format = 'jpeg'
+                    bg.save(filename=os.path.join(self.dir_path, self.thumbnail_uuid))
