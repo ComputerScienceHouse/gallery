3b265695b17174c415f83b98c0db497b8a80f332
Devin Matte <devinmatte@gmail.com>
2020-09-20 20:28:09 -0400

    Changing from sub to uuid

--- gallery/util.py
+++ gallery/util.py
@@ -69,9 +69,11 @@ def get_lockdown_status():
 def gallery_auth(func: Callable[..., Any]) -> Callable[..., Any]:
     @wraps(func)
     def wrapped_function(*args: Any, **kwargs: Any) -> Any:
-        uuid = str(session.get('userinfo', {}).get('sub', ''))
+        print(session.get('userinfo', {}))
+        uuid = str(session.get('userinfo', {}).get('uuid', ''))
         uid = str(session.get('userinfo', {}).get('preferred_username', ''))
         name = ldap.convert_uuid_to_displayname(uuid)
+        print(name)
         is_eboard = ldap.is_eboard(uid)
         is_rtp = ldap.is_rtp(uid)
 
